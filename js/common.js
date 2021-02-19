04e555ed53e53b72c05fb21d375835af43c597f8
jonio-uhei <73581065+jonio-uhei@users.noreply.github.com>
2021-02-19 13:35:04 +0900

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/*--------------------------------ここからハンバーガーメニュー------------------------------------------- */
+const hamburgerOpen = () => {
+  const line2 = document.getElementById('line2');
+  const line3 = document.getElementById('line3');
+  const nav = document.getElementById('nav');
+  line2.classList.toggle('line_2');
+  line3.classList.toggle('line_3');
+  nav.classList.toggle('in');
+};
+
+const hamburger = document.getElementById('hamburger');
+hamburger.addEventListener('click',() => {
+  hamburgerOpen();
+});
+/*--------------------------------ここまでハンバーガーメニュー------------------------------------------- */
+
+
+/*-------------------------------ここから「view more」を見ると現れるボタン---------------------------------------------*/
+$(function(){
+  $('.more-view').prevAll().hide();
+  $('.more-view').click(function(){
+    if($(this).prevAll().is(':hidden')){
+      $(this).prevAll().slideDown();
+      $('.more-view').hide();
+      $('.section-title').show();
+    }
+  })
+}); 
+/*---------------------------------ここまで「view more」------------------------------------------------*/
+
+
+
+
+
